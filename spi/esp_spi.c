aa360c5f0baa03936532809bd2eae8ed36526a59
虎子哥 <flashindream@gmail.com>
2023-05-14 16:38:27 +0800

    update

--- spi/esp_spi.c
+++ spi/esp_spi.c
@@ -511,7 +511,7 @@ static int spi_dev_init(int spi_clk_mhz)
 	strlcpy(esp_board.modalias, "esp32c3", sizeof(esp_board.modalias));
 	esp_board.mode = SPI_MODE_2;
 	esp_board.max_speed_hz = spi_clk_mhz * NUMBER_1M;
-	esp_board.bus_num = 0;
+	esp_board.bus_num = 1;
 	esp_board.chip_select = 1;
 
 	master = spi_busnum_to_master(esp_board.bus_num);
